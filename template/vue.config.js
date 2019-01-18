4b800159a75735c571b3c83588855a46a423c3ac
edwardorz <edwardyuguo@gmail.com>
2019-01-18 16:29:54 +0800

    update: target为lib时不单独打包css

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,7 +1,11 @@
+const argv = require('minimist')(process.argv.slice(2))
+
 module.exports = {
   productionSourceMap: false,
   publicPath: '/{{name}}',
   devServer: {
     open: true
-  }
+  },
+  css: { extract: argv.target !== 'lib' }
 }
+
